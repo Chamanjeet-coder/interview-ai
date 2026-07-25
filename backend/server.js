2b08daec00fbb0157e61be51976770ea0530132f
chamanjeet-coder <chamanjeets@gmail.com>
2026-07-25 19:45:39 +0530

    test4

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,6 @@ connectDB();//connect to Db
 // invokegeminiAi();
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`server is running on port ${PORT}`);
 });
